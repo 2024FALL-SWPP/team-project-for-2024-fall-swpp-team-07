580ccab2a9260f08f6f29889c1f6c9fa731e38fe
cntlgns <88645550+cntlgns@users.noreply.github.com>
2024-11-09 19:23:52 +0900

    add: edited PhysicsTester script

--- SAVE_THE_HAMSTER/Assets/Scripts/PhysicsTester.cs
+++ SAVE_THE_HAMSTER/Assets/Scripts/PhysicsTester.cs
@@ -24,5 +24,11 @@ public class PhysicsTester : MonoBehaviour
             Vector3 forceDirection = new Vector3(1f, 1f, 0f).normalized;
             rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
         }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // 수평 방향으로 힘을 가함
+            Vector3 forceDirection = new Vector3(1f, 0f, 0f);
+            rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+        }
     }
 }
