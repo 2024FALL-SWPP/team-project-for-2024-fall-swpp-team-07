8d5ef2f156414fdd3e70048c8d8685ead21485d2
cntlgns <88645550+cntlgns@users.noreply.github.com>
2024-11-15 23:15:02 +0900

    add: task 705: edited stage select scene and transition script

--- SAVE_THE_HAMSTER/Assets/Scripts/SceneTransitionManager.cs
+++ SAVE_THE_HAMSTER/Assets/Scripts/SceneTransitionManager.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransitionManager : MonoBehaviour
 {
+    private int stageNumber;
+    private StageSelector stageSelector;
     // Start is called before the first frame update
-    void Start() { }
+    void Start() 
+    {
+        stageSelector = FindObjectOfType<StageSelector>();
+    }
 
     // Update is called once per frame
-    void Update() { }
+    void Update() 
+    {    }
 
     // public void GoToStartScene()
     // {
@@ -31,9 +37,38 @@ public class SceneTransitionManager : MonoBehaviour
         SceneManager.LoadScene("StageSelectScene");
     }
 
+    public void GoToPreStageScene()
+    {
+        stageNumber = stageSelector.getStageNumber();
+        Debug.Log("GoToPreStageScene: " + stageNumber);
+        // SceneManager.LoadScene("PreStageScene");
+    }
+
     public void GoToStageScene()
     {
-        //SceneManager.LoadScene("StageScene");
+        switch (stageNumber)
+        {
+            case 1:
+                SceneManager.LoadScene("Stage1Scene");
+                break;
+            case 2:
+                SceneManager.LoadScene("Stage2Scene");
+                break;
+            case 3:
+                SceneManager.LoadScene("Stage3Scene");
+                break;
+            case 4:
+                SceneManager.LoadScene("Stage4Scene");
+                break;
+            default:
+                Debug.LogError("Invalid stage number: " + stageNumber);
+                break;
+        }
+    }
+
+    public void GoToGameOverScene()
+    {
+        SceneManager.LoadScene("GameOverScene");
     }
 
     public void GoToStoreScene()
---
+++ SAVE_THE_HAMSTER/Assets/Scripts/StageSelector.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // UI 컴포넌트 사용을 위해 추가
+
+public class StageSelector : MonoBehaviour
+{
+    private int stageNumber;
+    private float cameraPositionX;
+    public TMP_Text stageText;
+    // 스테이지 설명 추후 작성하기
+    private string [] stageExplanation = {"Stage1: ",
+                                          "Stage2: ",
+                                          "Stage3: ",
+                                          "Stage4: "};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stageNumber = 1;
+        cameraPositionX = -2.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && stageNumber > 1)
+        {
+            stageNumber--;
+            cameraPositionX -= 2.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && stageNumber < 4)
+        {
+            stageNumber++;
+            cameraPositionX += 2.0f;
+        }
+        transform.position = new Vector3(cameraPositionX, transform.position.y, transform.position.z);
+        UpdateStageText();
+    }
+
+    void UpdateStageText()
+    {
+        stageText.text = stageExplanation[stageNumber - 1];
+    }
+
+    public int getStageNumber()
+    {
+        return stageNumber;
+    }
+}
