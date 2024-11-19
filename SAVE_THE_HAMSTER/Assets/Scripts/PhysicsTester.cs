e8aa03f14b1b0213ede84b9f94cbc2c1dad47bb7
cntlgns <88645550+cntlgns@users.noreply.github.com>
2024-11-19 23:09:40 +0900

    add: task 202, 203: design tentatively complete, interaction implementation in progress

--- SAVE_THE_HAMSTER/Assets/Scripts/PhysicsTester.cs
+++ SAVE_THE_HAMSTER/Assets/Scripts/PhysicsTester.cs
@@ -19,31 +19,48 @@ public class PhysicsTester : MonoBehaviour
     void Update()
     {
         // 스페이스바를 누르면 힘을 가함
-        if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     // 45도 방향으로 힘을 가함 (x와 y를 1:1 비율로 설정)
+        //     Vector3 forceDirection = new Vector3(1f, 1f, 0f).normalized;
+        //     rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+        // }
+        if (Input.GetKey(KeyCode.Space))
         {
-            // 45도 방향으로 힘을 가함 (x와 y를 1:1 비율로 설정)
-            Vector3 forceDirection = new Vector3(1f, 1f, 0f).normalized;
-            rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                // 45도 방향으로 힘을 가함 (x와 y를 1:1 비율로 설정)
+                Vector3 forceDirection = new Vector3(1f, 1f, 0f).normalized;
+                rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                // 45도 방향으로 힘을 가함 (x와 y를 1:1 비율로 설정)
+                Vector3 forceDirection = new Vector3(0f, 1f, 1f).normalized;
+                rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+            }
+
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             // 수평 방향으로 힘을 가함
             Vector3 forceDirection = new Vector3(1f, 0f, 0f);
             rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             // 수평 방향으로 힘을 가함
             Vector3 forceDirection = new Vector3(-1f, 0f, 0f);
             rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             // 수평 방향으로 힘을 가함
             Vector3 forceDirection = new Vector3(0f, 0f, -1f);
             rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             // 수평 방향으로 힘을 가함
             Vector3 forceDirection = new Vector3(0f, 0f, 1f);
