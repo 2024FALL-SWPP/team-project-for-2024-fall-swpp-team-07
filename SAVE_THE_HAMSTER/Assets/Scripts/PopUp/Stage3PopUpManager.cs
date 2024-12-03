7cb33954d6b2bf5b74e53cb6bcd16740fda5ea44
cntlgns <88645550+cntlgns@users.noreply.github.com>
2024-12-04 04:34:59 +0900

    feat: task 209, 308: move popup object, canvas to orignal scene

--- SAVE_THE_HAMSTER/Assets/Scripts/PopUp/Stage3PopUpManager.cs
+++ SAVE_THE_HAMSTER/Assets/Scripts/PopUp/Stage3PopUpManager.cs
@@ -54,6 +54,8 @@ public class Stage3PopUpManager : PopUpManager
 
     private float Distance(GameObject environment)
     {
-        return Mathf.Abs(environment.transform.position.z - cannon.transform.position.z); //z축 차이만 비교
+        float distanceZ = Mathf.Abs(environment.transform.position.z - cannon.transform.position.z); //z축 차이만 비교
+        float distanceX = Mathf.Abs(environment.transform.position.x - cannon.transform.position.x); //x축 차이만 비교
+        return Mathf.Sqrt(distanceZ * distanceZ + distanceX * distanceX); // 두 점 사이의 거리 계산
     }
 }
