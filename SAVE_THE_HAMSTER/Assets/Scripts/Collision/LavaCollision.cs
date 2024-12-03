05bdb9213fe7787d6d0e85237ca8e6d80d33947e
cntlgns <88645550+cntlgns@users.noreply.github.com>
2024-12-03 14:46:31 +0900

    feat: task 202: add fire effect when lava collision

--- SAVE_THE_HAMSTER/Assets/Scripts/Collision/LavaCollision.cs
+++ SAVE_THE_HAMSTER/Assets/Scripts/Collision/LavaCollision.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LavaCollision : MonoBehaviour
 {
+
+    public ParticleSystem lavaCollisionParticle; // 인스펙터에서 파티클 시스템 할당
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,22 @@ public class LavaCollision : MonoBehaviour
     void Update()
     {
         
+    }
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // 충돌 지점 가져오기
+            ContactPoint contact = collision.contacts[0];
+            Vector3 collisionPoint = contact.point;
+            
+            // 파티클 위치를 충돌 지점으로 이동
+            lavaCollisionParticle.transform.position = collisionPoint;
+            
+            // 파티클 재생
+            lavaCollisionParticle.Play();
+        }
     }
 }
