2792b26b204f712ec0ca09f82c154b56cde5e92a
minseo25 <ms059333@gmail.com>
2024-12-18 02:27:14 +0900

    fix: 지인이 수정사항 (카메라 관련, collider 관련)

--- SAVE_THE_HAMSTER/Assets/Scripts/CameraControl.cs
+++ SAVE_THE_HAMSTER/Assets/Scripts/CameraControl.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+
 //using System.Numerics;
 using Cysharp.Threading.Tasks.Triggers;
 using JetBrains.Rider.Unity.Editor;
@@ -13,13 +15,17 @@ public class CameraControl : MonoBehaviour
     public Camera subCamera2;
     public GameObject cannon; //대포 전체
     public GameObject canon; //포신
+    public Transform firePoint; //탄환 발사 지점
     private GameObject activeBall;
     public Vector3 offset1;
     public Vector3 offset2;
-    public Vector3 offset3;
+    private Vector3 offset3;
     public Vector3 offset4;
     private float deltay = 3f;
 
+    private bool blockAlpha1;
+    private bool blockAlpha2;
+    private bool blockAlpha3;
     private bool isYLocked;
 
     private Vector3 mainLastPosition; //mainCamera의 이전 프레임 위치
@@ -34,19 +40,23 @@ public class CameraControl : MonoBehaviour
         cannonControl = cannon.GetComponent<CannonControl>();
         ActivateCamera1();
         mainLastPosition = mainCamera.transform.position;
+        blockAlpha1 = false;
+        blockAlpha2 = false;
+        blockAlpha3 = false;
+        isYLocked = false;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (!blockAlpha1 && Input.GetKeyDown(KeyCode.Alpha1))
         {
             ActivateCamera1(); //대포 바로 뒤 시점으로 전환
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (!blockAlpha2 && Input.GetKeyDown(KeyCode.Alpha2))
         {
             ActivateCamera2(); //대포 뒤 사선 시점으로 전환
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (!blockAlpha3 && Input.GetKeyDown(KeyCode.Alpha3))
         {
             ActivateCamera3(); //대포 위 시점으로 전환
         }
@@ -57,53 +67,48 @@ public class CameraControl : MonoBehaviour
     {
         if (cannon.activeSelf)
         {
-            activeBall = gm.GetActiveBall();
-            Rigidbody rb = activeBall.GetComponent<Rigidbody>();
-            subCamera1.transform.position = cannon.transform.position + offset2;
-            subCamera1.transform.LookAt(cannon.transform.position);
-
-            // 대포보다 살짝 위 (시야각 확보)
-            Vector3 desiredPosition2 =
-                canon.transform.position + canon.transform.rotation * offset4;
-            desiredPosition2.y += deltay;
-            subCamera2.transform.position = desiredPosition2;
-
-            // 포신을 바라보는 시점
-            Vector3 lookAtPosition = canon.transform.position;
-            lookAtPosition.y += deltay;
-            subCamera2.transform.LookAt(lookAtPosition);
-
-            //공 발사 전 mainCamera
-            if (cannonControl.spaceBarCount == 0)
+            if(cannonControl.spaceBarCount == 0)
             {
+                activeBall = gm.GetActiveBall();
+                Rigidbody rb = activeBall.GetComponent<Rigidbody>();
+                blockAlpha1 = false;
+                blockAlpha2 = false;
+                blockAlpha3 = false;
+                subCamera1.transform.position = cannon.transform.position + offset2;
+                subCamera1.transform.LookAt(cannon.transform.position);
+
+                // 대포보다 살짝 위 (시야각 확보)
+                Vector3 desiredPosition2 =
+                    canon.transform.position + canon.transform.rotation * offset4;
+                desiredPosition2.y += deltay;
+                subCamera2.transform.position = desiredPosition2;
+
+                // 포신을 바라보는 시점
+                Vector3 lookAtPosition = canon.transform.position;
+                lookAtPosition.y += deltay;
+                subCamera2.transform.LookAt(lookAtPosition);
+
+                //공 발사 전 mainCamera
                 isYLocked = false;
                 Vector3 desiredPosition1 =
                     cannon.transform.position + cannon.transform.rotation * offset1;
-
                 mainCamera.transform.position = desiredPosition1;
-
                 mainCamera.transform.rotation = cannon.transform.rotation;
-
                 mainCamera.transform.LookAt(cannon.transform.position);
             }
-            //공 발사 후 mainCamera
+        
             else
             {
+                activeBall = gm.GetActiveBall();
+                Rigidbody rb = activeBall.GetComponent<Rigidbody>();
+                blockAlpha1 = true;
+                blockAlpha2 = true;
+                blockAlpha3 = true;
                 ActivateCamera2();
-                if (rb.velocity.magnitude >= 1f && !isYLocked)
-                {
-                    mainCamera.transform.position = activeBall.transform.position + offset3;
-                    mainCamera.transform.LookAt(activeBall.transform.position);
-                }
-                else
-                {
-                    isYLocked = true;
-                    Vector3 newPosition = mainCamera.transform.position;
-                    newPosition.x = activeBall.transform.position.x + offset3.x;
-                    newPosition.z = activeBall.transform.position.z + offset3.z;
-                    mainCamera.transform.position = newPosition;
-                    mainCamera.transform.LookAt(activeBall.transform.position);
-                }
+                offset3 = new Vector3(5 * (canon.transform.position.x - firePoint.position.x), 5 * (firePoint.position.y - canon.transform.position.y) + 5f, 5 * (canon.transform.position.z - firePoint.position.z)) ;
+                mainCamera.transform.position = activeBall.transform.position + offset3;
+                Vector3 lookAtPosition = activeBall.transform.position;
+                mainCamera.transform.LookAt(lookAtPosition);
             }
         }
     }
